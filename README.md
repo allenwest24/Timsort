a3495afc5d7eda9669ae8c3d156fc0301b848bdd
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-07-14 20:38:44 -0400

    Upload without formatting

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# Timsort
-Timsort is cooler than mergesort.
+# Lab 2
+
+<img align="right" width="200px" src="./media/timsort.gif">
+
+> "Bentley Rules"
+
+In this lab you are going to implement and time some sorting algorithms, including Timsort in C++.
+
+# Implementation Logistics
+
+- You may use whatever operating system, IDE, or tools for completing this assignment.
+	- However, my instructions will usually be using the command-line, and that is what I will most easily be able to assist you with.
+- In the future there may be restrictions on what tools you may use, so please review the logistics each time.
+
+# Resources to help
+
+Provided below are a list of curated resources to help you complete the tasks below. Consult them (read them, or do ctrl+f for keywords) if you get stuck.
+
+1. https://www.youtube.com/watch?v=fHNmRkzxHWs
+	- Chandler Carruth "Efficiency with Algorithms, Performance with Data Structures"
+2. https://hackernoon.com/timsort-the-fastest-sorting-algorithm-youve-never-heard-of-36b28417f399
+	- Timsort algorithm reference.
+
+# Task 1 - Timsort
+
+In this lab, you are going to implement and time insertion sort, mergesort, and Timsort on a set of [data.txt](./data.txt) in [sort.cpp](./sort.cpp). Note: You may need to add additional headers for your imlpementations.
+
+This lab will also serve as a light self-guided tour into C++. You are free to organize the project in any way that you like, so long as I can compile with: `g++ -std=c++14 sort.cpp -o sort`
+
+You can toggle several things in this submission:
+- The resolution of the timer (currently in nanoseconds)
+- The data type of 'data' (currently assummed you will treat it as an integer)
+- The amount of data in 'data.txt' if you would like to add more or less.
+
+# Task 2 - Introduction to Performance
+
+Watch the following talk, [Efficiency with Algorithms, Performance with Data Structures](https://www.youtube.com/watch?v=fHNmRkzxHWs) and answer the questions in [exercise.md](./exercise.md)
+
+## Testing
+
+I will look at your [exercise.md](./exercise.md) to make sure you have filled out the answers.
+
+# Submission/Deliverables
+
+- A completed [exercise.md](./exercise.md).
+- A completed implementation of [sort.cpp](./sort.cpp)
+
+### Submission
+
+- Commit all of your files to github, including any additional files you create.
+- Do not commit any binary files unless told to do so.
+- Do not commit any 'data' files generated when executing a binary.
+
+# Rubric
+
+You (and any partner(s)) will receive the same grade from a scale of 0-2. Both partners should commit the same code.
+
+- 0 for no work completed by the deadline
+- 1 for some work completed, but something is not working properly
+- 2 for a completed lab (with possible 'going further' options completed)
+
+# Going Further
+
+An optional task(if any) that will reinforce your learning throughout the semester--this is not graded.
+
+1. Look up more of Chandler Carruth's talks--he is an expert in the LLVM framework which we will be using.
+
+# F.A.Q. (Instructor Anticipated Questions)
+
+1. Q: Do we just modify the exercises.md?
+- A: Yes
+2: Q: Can I separate out my class into multiple files?
+- A: Sure, you can amke a sort.h file if you like.
---
+++ sort.cpp
@@ -0,0 +1,155 @@
+// Compile with: g++ -std=c++14 sort.cpp -o sort 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include <chrono> // For timing
+
+using namespace std;
+
+// A C++ class
+class Sort{
+    public:
+    int numbers[16384];
+    
+    // The constructor
+    Sort(std:: string filename){
+        // Read in a file of data
+	ifstream myfile;
+        myfile.open(filename);
+        string line;
+        int curr = 0;
+        while (getline(myfile, line)) {
+                numbers[curr] = stoi(line);
+                curr += 1;
+        }
+
+        // Tutorial: http://www.cplusplus.com/doc/tutorial/files/
+    }
+
+    // Destructor
+    ~Sort(){
+    }
+
+    void insertionsortHelper(int arr[], int left, int right) {
+	int ii, jj, key;
+        for (ii = left + 1; ii <= right; ii++) {
+            key = arr[ii];
+            jj = ii - 1;
+            while (jj >= 0 && arr[jj] > key) {
+                arr[jj + 1] = arr[jj];
+                jj -=  1;
+            }
+            arr[jj + 1] = key;
+        }
+
+    }
+
+    // insertionsort: DONE!
+    void insertionsort(int arr[], int left, int right){
+        auto start = std::chrono::steady_clock::now();    
+	insertionsortHelper(arr, left, right);
+        auto end = std::chrono::steady_clock::now();
+        std::cout << "Elapsed time in nanoseconds : " 
+		<< std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
+		<< " ns" << std::endl;
+    }
+
+    void merge(int arr[], int left, int middle, int right) {
+    	int ii, jj, kk;
+    	int n1 = middle - left + 1;
+    	int n2 =  right - middle;
+    	int leftTemp[n1], rightTemp[n2];
+    	for (ii = 0; ii < n1; ii++) {
+            leftTemp[ii] = arr[left + ii];
+    	}
+    	for (jj = 0; jj < n2; jj++) {
+            rightTemp[jj] = arr[middle + 1 + jj];
+    	}
+    	ii = 0;
+    	jj = 0;
+    	kk = left;
+        while (ii < n1 && jj < n2) {
+            if (leftTemp[ii] <= rightTemp[jj]) {
+            	arr[kk] = leftTemp[ii];
+            	ii++;
+            }
+            else {
+                arr[kk] = rightTemp[jj];
+                jj++;
+            }
+            kk++;
+        }
+        while (ii < n1) {
+            arr[kk] = leftTemp[ii];
+            ii++;
+            kk++;
+        }
+        while (jj < n2) {
+            arr[kk] = rightTemp[jj];
+            jj++;
+            kk++;
+        }
+    }
+
+    void mergesortHelper(int arr[], int left, int right) {
+	if (left < right) {
+            int middle = left + (right - left) / 2;
+            mergesortHelper(arr, left, middle);
+            mergesortHelper(arr, middle + 1, right);
+            merge(arr, left, middle, right);
+        }
+    }
+
+
+    // mergesort: DONE!
+    void mergesort() {
+        auto start = std::chrono::steady_clock::now();    
+        mergesortHelper(this->numbers, 0, 9999);
+        auto end = std::chrono::steady_clock::now();
+        std::cout << "Elapsed time in nanoseconds : " 
+		<< std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
+		<< " ns" << std::endl;
+    }
+
+    // timsort: DONE!
+    void timsort() {
+        auto start = std::chrono::steady_clock::now();
+    	for (int ii = 0; ii < 1000; ii+=625) {
+            insertionsortHelper(this->numbers, ii, min(ii+624, 10000));
+        }
+        for (int size = 625; size < 10000; size = 2*size) {
+            for (int left = 0; left < 10000; left += 2*size) {
+                int mid = left + size - 1;
+                int right = min((left + 2*size - 1), (10000-1));
+                merge(this->numbers, left, mid, right);
+            }
+        }
+        auto end = std::chrono::steady_clock::now();
+        std::cout << "Elapsed time in nanoseconds : " 
+		<< std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
+		<< " ns" << std::endl;
+    }
+
+    private:
+    // Any private data member variables or functions here
+    // *hint* You will need to store in the data you read.
+
+};
+
+
+
+
+int main(){
+
+    Sort s1("data.txt");
+    Sort s2("data.txt");
+    Sort s3("data.txt");
+
+    s1.insertionsort(s1.numbers, 0, 10000);
+    s2.mergesort();
+    s3.timsort();
+
+    return 0;
+}
